706eef06814b69a976e0bfa267e864ff0e4819d4
Pedro <lobophf@gmail.com>
2022-02-13 16:03:11 -0300

    add controller

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -2,20 +2,11 @@ package com.example.accessingdatamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AccessingDataMysqlApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AccessingDataMysqlApplication.class, args);
   }
-
-  @GetMapping("/")
-  public String index() {
-    return "Hi";
-  }
-      
 }
