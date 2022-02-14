41d07487e62d7c3ededd44fe0a15a9926f2602aa
Pedro <lobophf@gmail.com>
2022-02-13 21:16:30 -0300

    CRUD

--- src/main/java/com/example/accessingdatamysql/controller/MainController.java
+++ src/main/java/com/example/accessingdatamysql/controller/MainController.java
@@ -1,16 +1,71 @@
 package com.example.accessingdatamysql.controller;
 
+import java.util.Optional;
+
+import javax.persistence.Id;
+
+import com.example.accessingdatamysql.models.User;
+import com.example.accessingdatamysql.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(path="/demo")
+@RequestMapping(path = "/demo")
 public class MainController {
 
-  @GetMapping(path="/")
-  public @ResponseBody String demo(){
+  @Autowired
+  private UserRepository userRepository;
+
+  //CREATE
+  @GetMapping(path = "/add")
+  public @ResponseBody String add(@RequestParam String name, @RequestParam String email) {
+    User n = new User();
+    n.setName(name);
+    n.setEmail(email);
+    userRepository.save(n);
+    return "Add";
+  }
+
+  //UPDATE
+  @GetMapping(path = "/update")
+  public @ResponseBody String update(@RequestParam String id, @RequestParam String name, @RequestParam String email) {
+
+    userRepository.deleteById(Integer.valueOf(id));
+
+    User n = new User();
+    n.setName(name);
+    n.setEmail(email);
+    userRepository.save(n);
+
+    return "update";
+  }
+
+  //READ
+  @GetMapping(path = "/read")
+  public @ResponseBody String read(@RequestParam int id) {
+    userRepository.findById(id);
+    return "Read";
+  }
+
+  //DELETE
+  @GetMapping(path = "/delete")
+  public @ResponseBody String delete(@RequestParam int id) {
+    userRepository.deleteById(id);
+    return "Delete";
+  }
+
+  @GetMapping(path = "/")
+  public @ResponseBody String demo() {
     return "Hi";
+  }
+
+  @GetMapping(path = "/all")
+  public @ResponseBody Iterable<User> getAllUsers() {
+    return userRepository.findAll();
   }
 }
---
+++ src/main/java/com/example/accessingdatamysql/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.accessingdatamysql.repositories;
+
+import com.example.accessingdatamysql.models.User;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+}
